c40379d14df830cbb8f420d1bd05dec65cc47d99
mar-mykle21 <travionpendergrass0@gmail.com>
2025-09-20 12:01:34 -0400

    added spam keywords list

---
+++ MarquesePendergrass_ProgrammingExercise_2.py
@@ -0,0 +1,7 @@
+
+# creates list of 30 common spam words to check in a given email
+SPAM_KEYWORDS = [
+    "free","urgent","instant","hurry","guaranteed","winner","cash","lottery","bonus","reward",
+    "won","jackpot","prize","credit","discount","wire","bank","billing","congrats","save",
+    "cheap","approved","income","debt","relief","dollar","dollars","guarantee","offer","limited-time"
+]
