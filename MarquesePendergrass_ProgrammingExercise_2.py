107f2a03d12131da347afcfee2db46c95b3bc173
mar-mykle21 <travionpendergrass0@gmail.com>
2025-09-20 12:11:20 -0400

    adds score_counter to count spam words

--- MarquesePendergrass_ProgrammingExercise_2.py
+++ MarquesePendergrass_ProgrammingExercise_2.py
@@ -5,3 +5,17 @@ SPAM_KEYWORDS = [
     "won","jackpot","prize","credit","discount","wire","bank","billing","congrats","save",
     "cheap","approved","income","debt","relief","dollar","dollars","guarantee","offer","limited-time"
 ]
+
+# Function used for counting the spam words in text
+def score_counter(text, words):
+    # creates a dictionary to store the found words and count
+    found = {}
+    score = 0
+    # for every letter in the word it will count the letter (making it lowercase)
+    # and add it to the count and 'found' words and then to the score
+    for w in words:
+        c = text.lower().count(w)
+        if c > 0:
+            found[w] = c
+            score += c
+    return score, found
