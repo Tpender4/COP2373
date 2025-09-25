f5d4797bbdf1d2b2dfd17f512ec86bc135fd42e3
mar-mykle21 <travionpendergrass0@gmail.com>
2025-09-25 17:11:46 -0400

    added reduce method import for later use and start of main method/program as well as technical document

---
+++ ProgrammingExercise3/MarquesePendergrass_ProgrammingExercise_3.py
@@ -0,0 +1,12 @@
+# Description: Program that prompts user to enter monthly expenses and then displays the total of expenses,
+# the highest expense listed and the lowest expense listed.
+
+# imports reduce method
+from functools import reduce
+
+def main():
+    # list to keep track of expenses the user enters
+    expenses = []
+
+    # Prompts user to enter the number of expenses they have
+    expenses_number = int(input("Enter the number of expenses you have: "))
